f9d759ed90a175432b6f9a480b9bab87c0318a9c
alexiordachescu <alex.iordachescu27@gmail.com>
2020-11-03 11:58:19 +0100

    inline selectors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useSelector } from "react-redux";
-import React from "react";
+import React, { useState } from "react";
 
 const getStatistics = (state) => {
   return {
@@ -9,13 +9,42 @@ const getStatistics = (state) => {
   };
 };
 
+const getResources = (state) => {
+  return state.resources;
+};
+
 function App() {
+  const resources = useSelector(getResources);
   const statistics = useSelector(getStatistics);
+  const [favoriteId, setFavoriteId] = useState(2);
+
+  const developersWithThisFavorite = useSelector((state) => {
+    return state.developers.filter((dev) => dev.favorites.includes(favoriteId));
+  });
+
   return (
     <div className="App">
       <h1>Web resources</h1>
       <div>Developers: {statistics.totalDevelopers}</div>
       <div>Resources: {statistics.totalResources}</div>
+
+      <select
+        value={favoriteId}
+        onChange={(e) => setFavoriteId(parseInt(e.target.value))}
+      >
+        {resources.map((resource) => {
+          return (
+            <option key={resource.id} value={resource.id}>
+              {resource.name}
+            </option>
+          );
+        })}
+      </select>
+      <ul>
+        {developersWithThisFavorite.map((dev) => {
+          return <li key={dev.id}>{dev.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
